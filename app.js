0112565cfb5688cd3f74439f450c79e47e1e1b10
Sarah E <seatherly.prsvr@gmail.com>
2023-12-05 02:47:21 -0600

    sendfile edit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
-const path = require('path')
+
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'index'))
+    res.sendFile('index')
 })
 
 function getTimestamp(req, res){

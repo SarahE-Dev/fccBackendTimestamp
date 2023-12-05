503998169ade591f093a546dceded9b14aedb7d2
Sarah E <seatherly.prsvr@gmail.com>
2023-12-05 02:49:19 -0600

    edit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.get('/', (req, res)=>{
-    res.sendFile('index')
+    res.sendFile('index.html')
 })
 
 function getTimestamp(req, res){

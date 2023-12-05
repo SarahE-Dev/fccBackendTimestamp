ca48162caa44b1f2be2d55636761b3a5aed052b3
Sarah E <seatherly.prsvr@gmail.com>
2023-12-05 01:51:49 -0600

    send file edit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.get('/', (req, res)=>{
-    res.sendFile('index.html')
+    res.sendFile(__dirname+'index.html')
 })
 
 app.get('/api/:date?', (req, res)=>{

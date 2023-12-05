1a6ed123de0cc626207ea0ef7c7ee86f583dda31
Sarah E <seatherly.prsvr@gmail.com>
2023-12-05 01:35:25 -0600

    Finished

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/timestamp/:date?', (req, res)=>{
+    const dateGiven = req.params.date;
+    let date;
+
+    if(!dateGiven){
+        date = new Date()
+    }else{
+        const unixCheck = dateGiven * 1;
+        if(isNaN(unixCheck)){
+            date = new Date(dateGiven)
+        }else{
+            date = new Date(unixCheck)
+        }
+    }
+
+    if(date === 'Invalid Date'){
+        res.json({error: date})
+    }else{
+        const unix = date.getTime();
+        const utc = date.toUTCString();
+        res.json({unix, utc})
+    }
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log('Server started on Port: 3000');
+})

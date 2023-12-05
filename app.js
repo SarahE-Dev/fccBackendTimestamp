4587d129e70d10a2038cbdcc9aec942d9e64e5db
Sarah E <seatherly.prsvr@gmail.com>
2023-12-05 02:13:07 -0600

    edit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
+const path = require('path')
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + 'index.html')
+    res.sendFile(path.join(__dirname, 'index'))
 })
 
 app.get('/api/:date?', (req, res)=>{
